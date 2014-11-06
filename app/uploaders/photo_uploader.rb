b28b08a56d7d884d044932dc9e0bdf77b0d08083
r.dubrovsky <roma2107946@gmail.com>
2014-11-06 12:52:02 +0300

    Resized images

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,5 +3,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
+
+  version :show do
+    resize_to_fit(800,600)
+  end
+
+  version :preview do
+    resize_to_fit(400,300)
+  end
   
 end
