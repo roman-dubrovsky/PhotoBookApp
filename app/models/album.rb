c1fad57a7c2fd467a20ba441879b06f246e78601
r.dubrovsky <roma2107946@gmail.com>
2014-11-06 12:30:40 +0300

    Refactored htmls and added simple header

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,5 @@ class Album < ActiveRecord::Base
   belongs_to :user
 
   has_many :album_photo_settings
-  has_many :photo, through: :album_photo_settings
+  has_many :photos, through: :album_photo_settings
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :user
 
   has_many :album_photo_settings
-  has_many :album, through: :album_photo_settings
+  has_many :albums, through: :album_photo_settings
 
   mount_uploader :file, PhotoUploader
 end
